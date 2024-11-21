005341a203e7a98f34eac026a49cfa0cdfd123bc
Ulric Scott <uscott.dev@.com>
2024-11-20 21:52:16 -0500

    Added API Key to Index method of Weather Controller, Created Index View, Updated API Call for Current City,

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,2 +1,7 @@
 class WeatherController < ApplicationController
+    def index
+        api_key = '884b65e5c1facd0cf9ce41c955079dd3'
+        api = WeatherApi.new(api_key)
+        @weather = api.weather_by_city('Atlanta')
+      end
 end
